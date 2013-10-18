8d871d7b2c5d912d571db7119244d7ea1a8d76c9
Phil Steer <pd@incb.us>
2013-10-18 10:42:55 +0100

    fixing up for adaptive/reponsive layout

--- js/main.js
+++ js/main.js
@@ -5,13 +5,14 @@
 $(document).ready(function() {
     $('ul.tree > li.closed').children('ul').hide();
     $('aside').find('ul.tree').hide().first().show();
+    $('aside').find('nav.controlsNav ul').hide().first().show();
 
     $(document).on('click', 'ul.tree > li > nav > h1', function() {
         if($(this).parents('li').last().hasClass('closed')) {
-            $(this).parents('li').last().removeClass('closed');
+            $(this).parents('li').last().removeClass('closed').addClass('active');
             $(this).parents('nav').siblings('ul').slideDown(200, 'easeOutQuad');
         } else {
-            $(this).parents('li').last().addClass('closed');
+            $(this).parents('li').last().addClass('closed').removeClass('active');
             $(this).parents('nav').siblings('ul').slideUp(200, 'easeOutQuad');
         }
     });
@@ -33,9 +34,11 @@ $(document).ready(function() {
     $('aside > nav.tabNav a').on('click', function(e) {
         e.preventDefault();
         var tab = $(this).attr('href');
+        var nav = tab.replace('#', '.');
         console.log(tab);
 
         $('aside').find('ul'+tab).show().siblings('ul.tree').hide();
+        $('aside').find('ul'+nav).show().siblings('ul').hide();
         $('aside nav.tabNav li a').removeClass('active');
         $(this).addClass('active');
     });
